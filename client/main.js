770c77b9173a5a05685c505f58dbd1f92648f9ae
Kelsey Taylor <kelseytay94@gmail.com>
2022-09-09 20:05:58 -0600

    committing get requests--insults, fortune

--- client/main.js
+++ client/main.js
@@ -1,11 +1,37 @@
-const complimentBtn = document.getElementById("complimentButton")
+const complimentBtn = document.getElementById("complimentButton");
+const insultBtn = document.getElementById("insultButton");
+const fortuneBtn = document.getElementById("fortuneButton");
 
 const getCompliment = () => {
-    axios.get("http://localhost:4000/api/compliment/")
-        .then(res => {
-            const data = res.data;
-            alert(data);
-    });
+	axios.get("http://localhost:4000/api/compliment/").then((res) => {
+		const data = res.data;
+		alert(data);
+	});
 };
 
-complimentBtn.addEventListener('click', getCompliment)
+const getInsult = () => {
+	axios.get("http://localhost:4000/api/insult/").then((res) => {
+		const data = res.data;
+		alert(data);
+	});
+};
+
+const getFortune = () => {
+	axios.get("http://localhost:4000/api/fortune").then((res) => {
+		const data = res.data;
+		alert(data);
+	});
+};
+
+const createBook = (body) => axios.post("http://localhost:4000/api/book", body).then();
+
+const submitHandler = (event) => {
+	event.preventDefault();
+
+	let title = document.querySelector("#title");
+	let imgURL = document.querySelector("#imgURL");
+};
+
+complimentBtn.addEventListener("click", getCompliment);
+insultBtn.addEventListener("click", getInsult);
+fortuneBtn.addEventListener("click", getFortune);
--- server/controller.js
+++ server/controller.js
@@ -1,13 +1,39 @@
+let books = require("./db.json");
+
 module.exports = {
+	getCompliment: (req, res) => {
+		const compliments = ["Gee, you're a smart cookie!", "Cool shirt!", "Your Javascript skills are stellar."];
 
-    getCompliment: (req, res) => {
-        const compliments = ["Gee, you're a smart cookie!", "Cool shirt!", "Your Javascript skills are stellar."];
-      
-        // choose random compliment
-        let randomIndex = Math.floor(Math.random() * compliments.length);
-        let randomCompliment = compliments[randomIndex];
-      
-        res.status(200).send(randomCompliment);
-    }
+		// choose random compliment
+		let randomIndex = Math.floor(Math.random() * compliments.length);
+		let randomCompliment = compliments[randomIndex];
 
-}
+		res.status(200).send(randomCompliment);
+	},
+
+	getInsult: (req, res) => {
+		const insults = ["You're less cool.", "You just aight.", "I like you, but I like your dog better."];
+
+		let randomIndex = Math.floor(Math.random() * insults.length);
+		let randomInsult = insults[randomIndex];
+
+		res.status(200).send(randomInsult);
+	},
+
+	getFortune: (req, res) => {
+		const fortune = [
+			//For you, Kyle :)
+			"Life before death.",
+			"Strength before weakness.",
+			"Journey before destination.",
+			"[You] will protect those who cannot protect themselves.",
+			"[You] will protect even those that [you] hate, so long as it is right.",
+			"[You] accept that there will be those [you] cannot protect.",
+		];
+
+		let randomIndex = Math.floor(Math.random() * fortune.length);
+		let randomFortune = fortune[randomIndex];
+
+		res.status(200).send(randomFortune);
+	},
+};
